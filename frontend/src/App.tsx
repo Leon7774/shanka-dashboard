a856ed26d89098fd426c03f85c5340f621473040
Leon <galileon.destura@gmail.com>
2025-12-17 01:04:56 +0800

    feat: add routes for dashboard and addsale

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Auth from './pages/Auth';
-import Dashboard from './pages/Dashboard';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "./pages/Auth";
+import Dashboard from "./pages/Dashboard";
+import AddSale from "./pages/AddSale";
+import Layout from "./components/Layout";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Auth />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Auth />} />
+                <Route element={<Layout />}>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/add-sale" element={<AddSale />} />
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
